92749aa7a77794bf5aa49b0f2004abee080b394d
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2022-10-29 00:09:00 +0200

    modeOption clean up on next visit

--- app/_component/_themeAble/_frame/_page/modeOptionPage/modeOptionPage.ts
+++ app/_component/_themeAble/_frame/_page/modeOptionPage/modeOptionPage.ts
@@ -19,16 +19,17 @@ export let wasHere = false
 
 class ModeOptionPage extends Page {
 
-
+  private initFigure: HTMLElement
+  private countdownElems: HTMLElement[]
   constructor(modeName: string) {
     super();
 
     doubleLink(db.msSettings[modeName], (this.body.msSetting as Input).value);
 
 
-    const initFigure = this.body.initFigure
+    const initFigure = this.initFigure = this.body.initFigure as any
     // @ts-ignore
-    const countdownElems = this.q("countdown-figure", true) as HTMLElement[];
+    const countdownElems = this.countdownElems = this.q("countdown-figure", true) as HTMLElement[];
     (this.body.form as Form).submit(async (e) => {
       let durInc = 500;
       await Promise.all([
@@ -43,13 +44,19 @@ class ModeOptionPage extends Page {
       domain.set("./run")
       // clean up
       setTimeout(() => {
-        initFigure.css({opacity: 1, translateY: 0})
         wasHere = false
       }, 300)
-      countdownElems.forEach((elem) => elem.css({opacity: 0, translateY: 0}))
+      
     });
 
     (this.body.form as Form).submitElement(this.body.goBtn as any)
+  }
+
+  activationCallback(active) {
+    if (active) {
+      this.initFigure.css({opacity: 1, translateY: 0})
+      this.countdownElems.forEach((elem) => elem.css({opacity: 0, translateY: 0}))
+    }
   }
 
 
